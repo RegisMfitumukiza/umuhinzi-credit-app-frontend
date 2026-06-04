61078e6cbe612c33d5a206977e2b5f3af0588d7e
Regis <mfitumukizaregis90@gmail.com>
2026-06-04 15:25:57 +0200

    refactor: change ui components to use real path

--- src/shared/components/ui/pagination.tsx
+++ src/shared/components/ui/pagination.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { Button } from "@/shared/components/ui/button"
 import { ChevronLeftIcon, ChevronRightIcon, MoreHorizontalIcon } from "lucide-react"
 
 function Pagination({ className, ...props }: React.ComponentProps<"nav">) {
@@ -64,12 +64,13 @@ function PaginationLink({
 function PaginationPrevious({
   className,
   text = "Previous",
+  size = "default",
   ...props
 }: React.ComponentProps<typeof PaginationLink> & { text?: string }) {
   return (
     <PaginationLink
       aria-label="Go to previous page"
-      size="default"
+      size={size}
       className={cn("pl-1.5!", className)}
       {...props}
     >
@@ -82,12 +83,13 @@ function PaginationPrevious({
 function PaginationNext({
   className,
   text = "Next",
+  size = "default",
   ...props
 }: React.ComponentProps<typeof PaginationLink> & { text?: string }) {
   return (
     <PaginationLink
       aria-label="Go to next page"
-      size="default"
+      size={size}
       className={cn("pr-1.5!", className)}
       {...props}
     >
